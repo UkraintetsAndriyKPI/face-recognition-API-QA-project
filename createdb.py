b78d7786f6630f86699fb82ab52293423114d4bc
AndriyUkrainetc <andriy.ukrainetc.school@gmail.com>
2024-11-01 13:21:42 +0200

    db managed only by migrations

--- createdb.py
+++
@@ -1,6 +0,0 @@
-from app import app, db  # Замініть 'your_application' на назву вашого файлу з Flask-додатком
-from models import User  # Якщо ваша модель User в окремому файлі
-
-# Створіть контекст додатку
-with app.app_context():
-    db.create_all()  # Це створить базу даних та таблиці
